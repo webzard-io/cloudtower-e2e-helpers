27e290b57899667b7120dd8ed998b7b1df0a0f12
Yanzhen Yu <yanzhen@smartx.com>
2023-09-19 15:27:55 +0800

    add wait helper

--- mod.ts
+++ mod.ts
@@ -1,1 +1,4 @@
 export { getOne } from "./get-one.ts";
+export { waitUntil } from "./wait.ts";
+
+export type { Wait } from "./wait.ts";
---
+++ wait.ts
@@ -0,0 +1,39 @@
+export type Wait = Partial<{
+  timeout: number;
+  interval: number;
+}>;
+
+export function waitUntil<TData>(
+  fn: () => Promise<TData>,
+  wait?: Wait
+): Promise<TData> {
+  const { timeout = 60, interval = 3 } = wait || {};
+  return new Promise((resolve, reject) => {
+    const start = Date.now();
+    const timeoutMs = timeout * 1000;
+
+    const callFn = async () => {
+      let err;
+      try {
+        const data = await fn();
+        if (data) {
+          clearInterval(timer);
+          resolve(data);
+          return;
+        }
+      } catch (error) {
+        err = error;
+      }
+
+      if (Date.now() - start > timeoutMs) {
+        clearInterval(timer);
+        reject(`timeout after ${timeout}s, error: ${err}`);
+      }
+    };
+
+    callFn();
+    const timer = setInterval(() => {
+      callFn();
+    }, interval * 1000);
+  });
+}
